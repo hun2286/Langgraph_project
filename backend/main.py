901641cd30ac5ec68e480f35c17bc8796acd9496
hun <hunminy0986@gmail.com>
2026-01-16 17:45:20 +0900

    host 수정

--- backend/main.py
+++ backend/main.py
@@ -48,4 +48,4 @@ async def chat(request: Request):
         )
 
 if __name__ == "__main__":
-    uvicorn.run("main:api_app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:api_app", host="0.0.0.0", port=8000, reload=True)
