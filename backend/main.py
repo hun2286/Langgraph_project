1b886b1219fdf8341174eeb60221a06e2eac78a4
hun <hunminy0986@gmail.com>
2026-01-28 17:19:33 +0900

    reload remove

--- backend/main.py
+++ backend/main.py
@@ -46,4 +46,4 @@ async def chat(request: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:api_app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:api_app", host="0.0.0.0", port=8000)
